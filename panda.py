7c34aebfca0c6259f82a4ec982dc25bf00df9ff7
spicyjuicy <mkerslake@thunderhead.com>
2019-01-15 21:19:56 +0000

    Fill2

--- panda.py
+++ panda.py
@@ -5,19 +5,30 @@ import matplotlib.pyplot as plt
 def fill_non_trading(dataframe):
     print(1)
     
+def setup_dataframe(file):
+
+    df = pd.read_csv(file)
+
+    df['Day_Name'] = pd.to_datetime(df['Date'])
+    print(df['Date'].iloc[0],df['Date'].iloc[-1])
+    idx = pd.date_range(df['Date'].iloc[0],df['Date'].iloc[-1])
+
+    df.index = pd.DatetimeIndex(df['Day_Name'])
+
+    df = df.reindex(idx)
 
 
 
+    df['Date'] = df.index
+    df = df.reset_index(drop=True)
+    df['Day_Name'] = pd.to_datetime(df['Date'])
+    df['DayNum'] = df['Day_Name'].dt.day
+    df['WeekNum'] = df['Day_Name'].dt.week
+    df['YearNum'] = df['Day_Name'].dt.year
 
+    #df['Difference'] = df['DayNum'].shift(-1) - df['DayNum']
 
-    
-df = pd.read_csv('FB.csv')
-
-df['Day_Name'] = pd.to_datetime(df['Date'])
-
-
-df['WeekNum'] = df['Day_Name'].dt.week
-df['YearNum'] = df['Day_Name'].dt.year
+    #df['rank'] = df.groupby(['YearNum','WeekNum'])['DayNum'].rank()
 #df = df.drop(columns=['Date','Day_Name'])
 
 
@@ -26,8 +37,12 @@ df['YearNum'] = df['Day_Name'].dt.year
 #        print(1)
 #        df[column] = df[column]/df[column].max()
 
-df.loc[0] = [np.random.randint(-1,1) for n in range(len(df.columns))]
+#df.loc[0] = [np.random.randint(-1,1) for n in range(len(df.columns))]
 
-print(df.head(25))
+    print(df.head(25))
 
-print(df.dtypes,len(df.columns))
+    print(df.dtypes,len(df.columns))
+
+file = 'FB.csv'
+
+setup_dataframe(file)
